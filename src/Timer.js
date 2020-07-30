1be559dee1c268500b85d96776ee976ca29b444b
sherotree <wytreeshero@163.com>
2020-07-30 20:50:55 +0800

    mmmm

--- src/Timer.js
+++ src/Timer.js
@@ -1,18 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 
 function Timer() {
-  const [time, setTime] = useState(
-    moment(new Date()).format('YYYY年MM月DD日 HH:mm:ss'),
-  )
+  const [time, setTime] = useState(moment(new Date()))
 
-  setInterval(
-    () => setTime(moment(new Date()).format('YYYY年MM月DD日 HH:mm:ss')),
-    1000,
-  )
+  useEffect(() => {
+    setInterval(() => setTime(moment(new Date())), 1000)
+  }, [])
 
-  console.log(time)
-  return <div>{time}</div>
+  return <div>{time.format('YYYY年MM月DD日 HH:mm:ss')}</div>
 }
 
 export default Timer
